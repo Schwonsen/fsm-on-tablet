78bdf7d60821ce660d291a3282b7179ec5db218a
maximlueck <maximlueck@googlemail.com>
2014-01-06 21:25:13 +0000

    Added new Activitys Added item menu to graph Activity Added new Icon

---
+++ FSMSim/src/com/uniks/fsmsim/GraphActivity.java
@@ -0,0 +1,45 @@
+package com.uniks.fsmsim;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.uniks.fsmsim.util.Message;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class GraphActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_graph);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.graph_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.item_save:
+	        	Message.message(this, "Automat wurde gespeichert!");
+	            return true;
+	        case R.id.item_load:
+	        	Message.message(this, "Automat wurde geladen!");	            
+	            return true;
+	        case R.id.item_new:
+	        	startActivity(new Intent(GraphActivity.this, GraphActivity.class));
+    			GraphActivity.this.finish();	
+                return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+}
